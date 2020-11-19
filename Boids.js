291db347480dc1d1eeb1995054a82d99ed719b19
RugvedPatil <72656927+RugvedPatil@users.noreply.github.com>
2020-11-19 16:49:03 +0530

    Add files via upload

---
+++ Boids.js
@@ -0,0 +1,216 @@
+let width = 1500;
+let height = 600;
+
+const numBoids = 150;
+const visualRange = 75 // Visual Range
+const centeringFactor = 0.007; // Cohesion
+const avoidFactor = 0.05; // Seperartion  
+const matchingFactor = 0.05; // Allignmen
+
+var boids = [];
+
+function initBoids() 
+{
+  for (var i = 0; i < numBoids; i += 1) {
+    boids[boids.length] = {
+      x: Math.random() * width,
+      y: Math.random() * height,
+      dx: Math.random() * 10 - 5,
+      dy: Math.random() * 10 - 5,
+      history: [],
+    };
+  }
+}
+function distance(boid1, boid2) 
+{
+  return Math.sqrt(
+    (boid1.x - boid2.x) * (boid1.x - boid2.x) +
+      (boid1.y - boid2.y) * (boid1.y - boid2.y),
+  );
+}
+
+function nClosestBoids(boid, n) 
+{
+  const sorted = boids.slice();
+  sorted.sort((a, b) => distance(boid, a) - distance(boid, b));
+  return sorted.slice(1, n + 1);
+}
+
+function sizeCanvas() 
+{
+  const canvas = document.getElementById("boids");
+  width = window.innerWidth/1.05;
+  height = window.innerHeight/1.5;
+  canvas.width = width;
+  canvas.height = height;
+}
+
+// Function to keep the boid folk within the frame of window
+function keepWithinBounds(boid)  
+{
+  const margin = 200;
+  const turnFactor = 0.75;
+
+  if (boid.x < margin) 
+  {
+    boid.dx += turnFactor;
+  }
+  if (boid.x > width - margin) 
+  {
+    boid.dx -= turnFactor
+  }
+  if (boid.y < margin) 
+  {
+    boid.dy += turnFactor;
+  }
+  if (boid.y > height - margin) 
+  {
+    boid.dy -= turnFactor;
+  }
+}
+
+function flyTowardsCenter(boid) // Cohesion
+{
+  let centerX = 0;
+  let centerY = 0;
+  let numNeighbors = 0;
+
+  for (let otherBoid of boids) 
+  {
+    if (distance(boid, otherBoid) < visualRange) 
+    {
+      centerX += otherBoid.x;
+      centerY += otherBoid.y;
+      numNeighbors += 1;
+    }
+  }
+
+  if (numNeighbors) 
+  {
+    centerX = centerX / numNeighbors;
+    centerY = centerY / numNeighbors;
+
+    boid.dx += (centerX - boid.x) * centeringFactor;
+    boid.dy += (centerY - boid.y) * centeringFactor;
+  }
+}
+
+function avoidOthers(boid) // Seperation 
+{
+  const minDistance = 20;
+  let moveX = 0;
+  let moveY = 0;
+  for (let otherBoid of boids) 
+  {
+    if (otherBoid !== boid) 
+    {
+      if (distance(boid, otherBoid) < minDistance) 
+      {
+        moveX += boid.x - otherBoid.x;
+        moveY += boid.y - otherBoid.y;
+      }
+    }
+  }
+
+  boid.dx += moveX * avoidFactor;
+  boid.dy += moveY * avoidFactor;
+}
+
+function matchVelocity(boid) // Allignment
+{
+  let avgDX = 0;
+  let avgDY = 0;
+  let numNeighbors = 0;
+
+  for (let otherBoid of boids) 
+  {
+    if (distance(boid, otherBoid) < visualRange) 
+    {
+      avgDX += otherBoid.dx;
+      avgDY += otherBoid.dy;
+      numNeighbors += 1;
+    }
+  }
+
+  if (numNeighbors) 
+  {
+    avgDX = avgDX / numNeighbors;
+    avgDY = avgDY / numNeighbors;
+
+    boid.dx += (avgDX - boid.dx) * matchingFactor;
+    boid.dy += (avgDY - boid.dy) * matchingFactor;
+  }
+}
+
+function limitSpeed(boid) 
+{
+  const speedLimit = 15;
+
+  const speed = Math.sqrt(boid.dx * boid.dx + boid.dy * boid.dy);
+  if (speed > speedLimit) {
+    boid.dx = (boid.dx / speed) * speedLimit;
+    boid.dy = (boid.dy / speed) * speedLimit;
+  }
+}
+
+const DRAW_TRAIL = false;
+
+function drawBoid(ctx, boid) 
+{
+  const angle = Math.atan2(boid.dy, boid.dx);
+  ctx.translate(boid.x, boid.y);
+  ctx.rotate(angle);
+  ctx.translate(-boid.x, -boid.y);
+  ctx.fillStyle = "#ffffff";
+  ctx.beginPath();
+  ctx.moveTo(boid.x, boid.y);
+  ctx.lineTo(boid.x - 15, boid.y + 5);
+  ctx.lineTo(boid.x - 15, boid.y - 5);
+  ctx.lineTo(boid.x, boid.y);
+  ctx.fill();
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+  if (DRAW_TRAIL) 
+  {
+    ctx.strokeStyle = "#cfcfcf66";
+    ctx.beginPath();
+    ctx.moveTo(boid.history[0][0], boid.history[0][1]);
+    for (const point of boid.history) 
+    {
+      ctx.lineTo(point[0], point[1]);
+    }
+    ctx.stroke();
+  }
+}
+
+function animationLoop() 
+{
+  for (let boid of boids) 
+  {
+    flyTowardsCenter(boid);
+    avoidOthers(boid);
+    matchVelocity(boid);
+    limitSpeed(boid);
+    keepWithinBounds(boid);
+
+    boid.x += boid.dx;
+    boid.y += boid.dy;
+    boid.history.push([boid.x, boid.y])
+    boid.history = boid.history.slice(-50);
+  }
+  const ctx = document.getElementById("boids").getContext("2d");
+  ctx.clearRect(0, 0, width, height);
+  for (let boid of boids) 
+  {
+    drawBoid(ctx, boid);
+  }
+
+  window.requestAnimationFrame(animationLoop);
+}
+
+window.onload = () => {
+  window.addEventListener("resize", sizeCanvas, false);
+  sizeCanvas();
+  initBoids();
+  window.requestAnimationFrame(animationLoop);
+};
